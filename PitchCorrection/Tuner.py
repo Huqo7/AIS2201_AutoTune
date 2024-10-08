5741d8da851e0c30d71f40bd8e3a135198d7bccf
Huqo7 <vegfor07@gmail.com>
2024-10-08 14:55:43 +0200

    updated tuner added notebook

--- PitchCorrection/Tuner.py
+++ PitchCorrection/Tuner.py
@@ -4,13 +4,13 @@ import scipy.signal as sig
 import sounddevice as sd
 from scipy.io import wavfile
 
-fs, data = wavfile.read("AudioFile/Actual.wav")
+fs, data = wavfile.read("AudioFile/Beeps.wav")
 
 if len(data.shape) > 1:
     data = data[:, 0]
 data = data / max(abs(data))
 
-window_size = int(fs / 40)
+window_size = int(fs / 80)
 overlap_size = window_size // 2
 
 window_func = np.hanning(window_size)
@@ -26,21 +26,58 @@ for begin in range(0, total_samples - window_size, overlap_size):
     window = window_square * window_func
     window_freq = np.fft.rfft(window)
 
-    frequency = np.argmax(window_freq) * fs / window_size
-    n = np.round(12*np.log2(frequency/440))
-    correct_frequency = np.pow(2,(n/12))*440
-
-    print(frequency)
-    adjuster = 1
-    if frequency != 0:
-        adjuster = correct_frequency/frequency
-    adjusted_window = np.fft.rfft(window, n=int(window_size*adjuster))
-    new_window = np.fft.irfft(adjusted_window, n=window_size)
+    new_window = np.fft.irfft(window_freq, n=window_size)
     new_audio[output_index : output_index + window_size] += new_window
     output_index += overlap_size
 
-plt.close(1)
-plt.figure(1)
+f_axis, t_axis, S_xx = sig.spectrogram(data, 
+                             fs, 
+                             window = 'hann', 
+                             nperseg = window_size,
+                             noverlap = overlap_size,
+                             nfft = 2*window_size
+                            )
+
+f_axis2, t_axis2, S_xx2 = sig.spectrogram(new_audio, 
+                             fs, 
+                             window = 'hann', 
+                             nperseg = window_size,
+                             noverlap = overlap_size,
+                             nfft = 2*window_size
+                            )
+
+plt.close(1);plt.figure(1, figsize=(10,5))
+plt.subplot(2,1,1)
+plt.pcolormesh(t_axis,            # Array med tidsstempel. Produsert av spectrogram()
+               f_axis,            # Array med frekvenser. Produsert av spectrogram()
+               10*np.log10(S_xx),  # Konvertering av spektrogrammatrise til logaritmisk skala
+               vmax =-40,         # Høyeste dB-verdi på fargekartet
+               vmin = -70,        # Laveste dB-verdi på fargekartet
+               shading='auto'
+               )
+plt.colorbar(aspect=50, label=r'Spectral Density (dB/Hz)')
+plt.ylim([0, 3000]) # Du kan justere på grensene i y-aksen dersom du vil inspisere en spesiell del av plottet
+#plt.xlim([21.5, 23.5]) # Du kan justere på grensene i x-aksen dersom du vil inspisere en spesiell del av plottet
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.subplot(2,1,2)
+plt.pcolormesh(t_axis2,            # Array med tidsstempel. Produsert av spectrogram()
+               f_axis2,            # Array med frekvenser. Produsert av spectrogram()
+               10*np.log10(S_xx2),  # Konvertering av spektrogrammatrise til logaritmisk skala
+               vmax =-40,         # Høyeste dB-verdi på fargekartet
+               vmin = -70,        # Laveste dB-verdi på fargekartet
+               shading='auto'
+               )
+plt.colorbar(aspect=50, label=r'Spectral Density (dB/Hz)')
+plt.ylim([0, 3000]) # Du kan justere på grensene i y-aksen dersom du vil inspisere en spesiell del av plottet
+#plt.xlim([21.5, 23.5]) # Du kan justere på grensene i x-aksen dersom du vil inspisere en spesiell del av plottet
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.tight_layout(pad=0.1, w_pad=1.0, h_pad=1.0)
+plt.show()
+
+plt.close(2)
+plt.figure(2)
 plt.subplot(2, 1, 1)
 plt.plot(data)
 plt.subplot(2, 1, 2)
